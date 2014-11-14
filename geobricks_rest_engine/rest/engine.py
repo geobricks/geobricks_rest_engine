c8219621fcb0d440d8a34f8995f2a74e4f69f49b
Kalimaha <guido.barbaglia@gmail.com>
2014-11-14 11:45:14 +0100

    Added generic discovery service and discovery by type service.

--- geobricks_rest_engine/rest/engine.py
+++ geobricks_rest_engine/rest/engine.py
@@ -38,7 +38,24 @@ def discovery():
         if '.' in r.endpoint and rule_name.endswith('/') and 'discovery' in rule_name:
             discovery_url = request.host_url + rule_name[1:]
             plugin_description = json.load(urllib2.urlopen(discovery_url))
-            if 'DATASOURCE' in plugin_description['type'].upper():
+            rules.append(plugin_description)
+    rules.sort()
+    return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
+
+@app.route('/discovery/<type>/')
+@cross_origin(origins='*')
+def discovery_by_type(type):
+    """
+    Discovery service to list all the available Geobricks plug-ins.
+    @return: List of objects describing the plug-in: name, description and type.
+    """
+    rules = []
+    for r in app.url_map.iter_rules():
+        rule_name = str(r)
+        if '.' in r.endpoint and rule_name.endswith('/') and 'discovery' in rule_name:
+            discovery_url = request.host_url + rule_name[1:]
+            plugin_description = json.load(urllib2.urlopen(discovery_url))
+            if type.upper() in plugin_description['type'].upper():
                 rules.append(plugin_description)
     rules.sort()
     return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
