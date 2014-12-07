521662dad66ea3e2512078787b2f354494c1b3cb
Guido Barbaglia <guido.barbaglia@gmail.com>
2014-12-07 21:17:39 +0100

    Rationalization of the calls to one module.

--- geobricks_rest_engine/rest/engine.py
+++ geobricks_rest_engine/rest/engine.py
@@ -10,16 +10,14 @@ from flask.ext.cors import cross_origin
 from geobricks_rest_engine.config.rest_settings import settings as rest_settings
 from geobricks_rest_engine.config.common_settings import settings as common_settings
 from geobricks_rest_engine.core.utils import dict_merge
-from geobricks_rest_engine.core.log import logger
 
-# Logger
-log = logger(__file__)
-
-# werkzeug logging level.
-logging.getLogger('werkzeug').setLevel(logging.INFO)
 
 # Initialize the Flask app
 app = Flask(__name__)
+
+# Logging level.
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 # Initialize CORS filters
 cors = CORS(app, resources={r'/*': {'origins': '*', 'headers': ['Content-Type']}})
@@ -27,24 +25,19 @@ cors = CORS(app, resources={r'/*': {'origins': '*', 'headers': ['Content-Type']}
 # Dynamic import of modules specified in config.settings.py
 for module in rest_settings['modules']:
 
-    try:
-        # Load module
-        mod = import_module(module['path_to_the_blueprint'])
+    # Load module
+    mod = import_module(module['path_to_the_blueprint'])
 
-        # Overwrite modules settings
-        conf_mod = import_module(module['path_to_the_config'])
+    # Overwrite modules settings
+    conf_mod = import_module(module['path_to_the_config'])
+    conf = getattr(conf_mod, 'config')
+    conf['common_settings'] = dict_merge(conf, common_settings)
 
-        # conf = conf_mod.config
-        conf_mod.config["settings"] = dict_merge(conf_mod.config, common_settings)
-        conf_mod.config["settings"] = conf_mod.config["settings"]["settings"]
+    # Load Blueprint
+    rest = getattr(mod, module['blueprint_name'])
 
-        # Load Blueprint
-        rest = getattr(mod, module['blueprint_name'])
-
-        # Register Blueprint
-        app.register_blueprint(rest, url_prefix=module['url_prefix'])
-    except Exception, e:
-        log.error(e)
+    # Register Blueprint
+    app.register_blueprint(rest, url_prefix=module['url_prefix'])
 
 @app.route('/')
 @cross_origin(origins='*')
@@ -62,33 +55,31 @@ def discovery():
     Discovery service to list all the available Geobricks plug-ins.
     @return: List of objects describing the plug-in: name, description and type.
     """
-    rules = []
-    for r in app.url_map.iter_rules():
-        rule_name = str(r)
-        if '.' in r.endpoint and rule_name.endswith('/') and 'discovery' in rule_name:
-            discovery_url = request.host_url + rule_name[1:]
-            plugin_description = json.load(urllib2.urlopen(discovery_url))
-            rules.append(plugin_description)
-    rules.sort()
+    rules = get_rules_by_type(None)
     return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
 
-@app.route('/discovery/<type>/')
+@app.route('/discovery/<rule_type>/')
 @cross_origin(origins='*')
-def discovery_by_type(type):
+def discovery_by_type(rule_type):
     """
     Discovery service to list all the available Geobricks plug-ins.
     @return: List of objects describing the plug-in: name, description and type.
     """
+    rules = get_rules_by_type(rule_type)
+    return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
+
+
+def get_rules_by_type(rule_type):
     rules = []
     for r in app.url_map.iter_rules():
         rule_name = str(r)
         if '.' in r.endpoint and rule_name.endswith('/') and 'discovery' in rule_name:
             discovery_url = request.host_url + rule_name[1:]
             plugin_description = json.load(urllib2.urlopen(discovery_url))
-            if type.upper() in plugin_description['type'].upper():
+            if rule_type is None or rule_type.upper() in plugin_description['type'].upper():
                 rules.append(plugin_description)
     rules.sort()
-    return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
+    return rules
 
 
 # Start Flask server
