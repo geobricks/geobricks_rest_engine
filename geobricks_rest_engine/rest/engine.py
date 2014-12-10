8e5297aab38a46d56c25a5a5f087d49de4cd81ee
vortex <simone.murzilli@fao.org>
2014-12-10 12:31:04 +0100

    fix on rest engine

--- geobricks_rest_engine/rest/engine.py
+++ geobricks_rest_engine/rest/engine.py
@@ -10,14 +10,16 @@ from flask.ext.cors import cross_origin
 from geobricks_rest_engine.config.rest_settings import settings as rest_settings
 from geobricks_rest_engine.config.common_settings import settings as common_settings
 from geobricks_rest_engine.core.utils import dict_merge
+from geobricks_rest_engine.core.log import logger
 
+# Logger
+log = logger(__file__)
+
+# werkzeug logging level.
+logging.getLogger('werkzeug').setLevel(logging.INFO)
 
 # Initialize the Flask app
 app = Flask(__name__)
-
-# Logging level.
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
 
 # Initialize CORS filters
 cors = CORS(app, resources={r'/*': {'origins': '*', 'headers': ['Content-Type']}})
@@ -25,19 +27,24 @@ cors = CORS(app, resources={r'/*': {'origins': '*', 'headers': ['Content-Type']}
 # Dynamic import of modules specified in config.settings.py
 for module in rest_settings['modules']:
 
-    # Load module
-    mod = import_module(module['path_to_the_blueprint'])
+    try:
+        # Load module
+        mod = import_module(module['path_to_the_blueprint'])
 
-    # Overwrite modules settings
-    conf_mod = import_module(module['path_to_the_config'])
-    conf = getattr(conf_mod, 'config')
-    conf['common_settings'] = dict_merge(conf, common_settings)
+        # Overwrite modules settings
+        conf_mod = import_module(module['path_to_the_config'])
 
-    # Load Blueprint
-    rest = getattr(mod, module['blueprint_name'])
+        # conf = conf_mod.config
+        conf_mod.config["settings"] = dict_merge(conf_mod.config, common_settings)
+        conf_mod.config["settings"] = conf_mod.config["settings"]["settings"]
 
-    # Register Blueprint
-    app.register_blueprint(rest, url_prefix=module['url_prefix'])
+        # Load Blueprint
+        rest = getattr(mod, module['blueprint_name'])
+
+        # Register Blueprint
+        app.register_blueprint(rest, url_prefix=module['url_prefix'])
+    except Exception, e:
+        log.error(e)
 
 @app.route('/')
 @cross_origin(origins='*')
@@ -55,33 +62,36 @@ def discovery():
     Discovery service to list all the available Geobricks plug-ins.
     @return: List of objects describing the plug-in: name, description and type.
     """
-    rules = get_rules_by_type(None)
-    return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
-
-@app.route('/discovery/<rule_type>/')
-@cross_origin(origins='*')
-def discovery_by_type(rule_type):
-    """
-    Discovery service to list all the available Geobricks plug-ins.
-    @return: List of objects describing the plug-in: name, description and type.
-    """
-    rules = get_rules_by_type(rule_type)
-    return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
-
-
-def get_rules_by_type(rule_type):
     rules = []
     for r in app.url_map.iter_rules():
         rule_name = str(r)
         if '.' in r.endpoint and rule_name.endswith('/') and 'discovery' in rule_name:
             discovery_url = request.host_url + rule_name[1:]
             plugin_description = json.load(urllib2.urlopen(discovery_url))
-            if rule_type is None or rule_type.upper() in plugin_description['type'].upper():
+            rules.append(plugin_description)
+    rules.sort()
+    return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
+
+@app.route('/discovery/<type>/')
+@cross_origin(origins='*')
+def discovery_by_type(type):
+    """
+    Discovery service to list all the available Geobricks plug-ins.
+    @return: List of objects describing the plug-in: name, description and type.
+    """
+    rules = []
+    for r in app.url_map.iter_rules():
+        rule_name = str(r)
+        if '.' in r.endpoint and rule_name.endswith('/') and 'discovery' in rule_name:
+            discovery_url = request.host_url + rule_name[1:]
+            plugin_description = json.load(urllib2.urlopen(discovery_url))
+            if type.upper() in plugin_description['type'].upper():
                 rules.append(plugin_description)
     rules.sort()
-    return rules
+    return Response(json.dumps(rules), content_type='application/json; charset=utf-8')
 
 
 # Start Flask server
 if __name__ == '__main__':
+    print rest_settings
     app.run(host=rest_settings['host'], port=rest_settings['port'], debug=rest_settings['debug'], threaded=True)
