e547f5914a567f9064c20847f00c2b90f1fcf395
simonefenix <simone.murzilli@fao.org>
2015-03-16 14:58:37 +0100

    added watchdog for flask werzug issue

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     long_description=open('README.md').read(),
     description='Common entry point for Geobricks REST services.',
     install_requires=[
+        'watchdog',
         'flask',
         'flask-cors',
         'importlib'
